ded18235d6512c95045b68777023ad5eaffba37c
Yanina0704 <yanina0704@gmail.com>
2024-06-23 22:40:50 -0700

    Got products list from dynamodb

--- product-service/lambdaF/getProductsList.js
+++ product-service/lambdaF/getProductsList.js
@@ -1,17 +1,37 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb");
+
 exports.handler = async(event, context, callback) => {
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+  products = []
+
+  const products_list = new ScanCommand({
+    TableName: 'products',
+  });
+
+  const stocks_list = new ScanCommand({
+    TableName: 'stocks',
+  });
+
+  const response1 = await docClient.send(products_list);
+  const response2 = await docClient.send(stocks_list);
+  for (const item of response1.Items) {
+    stock_item = response2.Items.find((element) => element["product_id"] == item['id']);
+    product = {'id': item['id'],
+      'title': item['title'],
+      'title': item['title'],
+      'description': item['description'],
+      'price': item['price'],
+      'count': stock_item['count']
+    };
+    products.push(product);
+  }
+
     return {
       statusCode: 200,
-      body: JSON.stringify([
-      {
-        id: "1",
-        title: "Milk Chocolate",
-        price: 10,
-      },
-      {
-        id: "2",
-        title: "Book Story",
-        price: 123,
-      }]),
+      body: JSON.stringify(products),
       headers: {
         "Access-Control-Allow-Headers" : "Content-Type",
         "Access-Control-Allow-Origin": "*",
--- product-service/product_service/product_service_stack.py
+++ product-service/product_service/product_service_stack.py
@@ -4,8 +4,17 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigateway,
+    RemovalPolicy as policy,
+    aws_dynamodb as dynamodb,
     # aws_sqs as sqs,
 )
+from aws_cdk.aws_dynamodb import(
+    Table,
+    TableAttributes,
+    AttributeType,
+    TableEncryption,
+)
+
 from constructs import Construct
 from product_service.populate_table import write_items
 
@@ -16,12 +25,23 @@ class ProductServiceStack(Stack):
 
         # The code that defines your stack goes here
 
+        products_table_name = 'products'
+        stocks_table_name = 'stocks'
+
+        products_table = dynamodb.Table.from_table_name(self, 'ProductsTable', products_table_name)
+                               
+        stocks_table = dynamodb.Table.from_table_name(self, 'StocksTable', stocks_table_name)
+
+        environment = { 'PRODUCTS_TABLE_NAME': products_table_name,
+                        'STOCKS_TABLE_NAME': stocks_table_name}
+
         get_products_list_function = _lambda.Function(
             self,
             "GetProductsListHandler",
             runtime = _lambda.Runtime.NODEJS_20_X, # Choose any supported Node.js runtime
             code = _lambda.Code.from_asset("lambdaF"), # Points to the lambdaF directory
             handler = "getProductsList.handler", # Points to the 'getProductList' file in the lambda directory
+            environment = environment,
         )
 
         get_product_by_id_function = _lambda.Function(
@@ -30,6 +50,7 @@ class ProductServiceStack(Stack):
             runtime = _lambda.Runtime.NODEJS_20_X, # Choose any supported Node.js runtime
             code = _lambda.Code.from_asset("lambdaF"), # Points to the lambdaF directory
             handler = "getProductById.handler", # Points to the 'getProductList' file in the lambda directory
+            environment = environment,
         )
         
         #write_items()
@@ -50,6 +71,11 @@ class ProductServiceStack(Stack):
         
         product_by_id_resource = products_resource.add_resource("{productId}")
         product_by_id_resource.add_method("GET", apigateway.LambdaIntegration(get_product_by_id_function))
+
+        products_table.grant_read_write_data(get_products_list_function)
+        products_table.grant_read_write_data(get_product_by_id_function)
+        stocks_table.grant_read_write_data(get_products_list_function)
+        stocks_table.grant_read_write_data(get_product_by_id_function)
 
         # example resource
         # queue = sqs.Queue(
