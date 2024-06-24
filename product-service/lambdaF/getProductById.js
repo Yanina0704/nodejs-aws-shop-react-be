a7ff2897358b38ccd7179ea858d8cc75771be958
Yanina0704 <yanina0704@gmail.com>
2024-06-23 22:58:55 -0700

    Got product from dynamodb

--- product-service/lambdaF/getProductById.js
+++ product-service/lambdaF/getProductById.js
@@ -1,21 +1,26 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb");
+
 exports.handler = async (event) => {
   try{
-    products = [
-      {
-        id: "1",
-        title: "Milk Chocolate",
-        price: 10,
-      },
-      {
-        id: "2",
-        title: "Book Story",
-        price: 123,
-      }];
-      productId = event['pathParameters']['productId'];
-      product = products.find((element) => element["id"] == productId);
-      if (!product)
-        {
-          return {
+    const client = new DynamoDBClient({});
+    const docClient = DynamoDBDocumentClient.from(client);
+
+    const products_list = new ScanCommand({
+      TableName: 'products',
+    });
+
+    const stocks_list = new ScanCommand({
+      TableName: 'stocks',
+    });
+    productId = event['pathParameters']['productId'];
+    const response1 = await docClient.send(products_list);
+    const response2 = await docClient.send(stocks_list);
+    product_item = response1.Items.find((element) => element["id"] == productId);
+    stock_item = response2.Items.find((element) => element["product_id"] == productId);
+    
+    if (!product_item){
+      return {
             statusCode: 404,
             body: JSON.stringify({title: "Product doesn't found!"}),
             headers: {
@@ -25,6 +30,12 @@ exports.handler = async (event) => {
           },
           }}
           else {
+            product = {'id': product_item['id'],
+              'title': product_item['title'],
+              'title': product_item['title'],
+              'description': product_item['description'],
+              'price': product_item['price'],
+              'count': stock_item['count']};
             return {
               statusCode: 200,
               body: JSON.stringify(product),
--- product-service/lambdaF/getProductsList.js
+++ product-service/lambdaF/getProductsList.js
@@ -2,40 +2,52 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb");
 
 exports.handler = async(event, context, callback) => {
-const client = new DynamoDBClient({});
-const docClient = DynamoDBDocumentClient.from(client);
+  try{
+    const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
 
-  products = []
+    products = []
 
-  const products_list = new ScanCommand({
-    TableName: 'products',
-  });
+    const products_list = new ScanCommand({
+      TableName: 'products',
+    });
 
-  const stocks_list = new ScanCommand({
-    TableName: 'stocks',
-  });
+    const stocks_list = new ScanCommand({
+      TableName: 'stocks',
+    });
 
-  const response1 = await docClient.send(products_list);
-  const response2 = await docClient.send(stocks_list);
-  for (const item of response1.Items) {
-    stock_item = response2.Items.find((element) => element["product_id"] == item['id']);
-    product = {'id': item['id'],
-      'title': item['title'],
-      'title': item['title'],
-      'description': item['description'],
-      'price': item['price'],
-      'count': stock_item['count']
-    };
-    products.push(product);
-  }
+    const response1 = await docClient.send(products_list);
+    const response2 = await docClient.send(stocks_list);
+    for (const item of response1.Items) {
+      stock_item = response2.Items.find((element) => element["product_id"] == item['id']);
+      product = {'id': item['id'],
+        'title': item['title'],
+        'title': item['title'],
+        'description': item['description'],
+        'price': item['price'],
+        'count': stock_item['count']
+      };
+      products.push(product);
+    }
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(products),
-      headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET"
-    },
-  };
+      return {
+        statusCode: 200,
+        body: JSON.stringify(products),
+        headers: {
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET"
+      },
+    };}
+    catch(error)
+          {
+            return{
+              statusCode: 500,
+              body: JSON.stringify(error.message),
+              headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET"
+            },};
+          }
   }
